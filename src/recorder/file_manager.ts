8c776852d755453e0dc5e98ad61faedde253f82a
ww24 <ww24@users.noreply.github.com>
2026-04-04 01:54:43 +0900

    refactor: remove directory caching from FileManager

--- src/recorder/file_manager.ts
+++ src/recorder/file_manager.ts
@@ -2,20 +2,11 @@ import type { ContainerFormat } from '../configuration'
 import { containerExtension, Configuration } from '../configuration'
 
 export class FileManager {
-    private dirHandle: FileSystemDirectoryHandle | undefined
-
-    private async getDirectory(): Promise<FileSystemDirectoryHandle> {
-        if (this.dirHandle == null) {
-            this.dirHandle = await navigator.storage.getDirectory()
-        }
-        return this.dirHandle
-    }
-
     /**
      * Create a recording file in OPFS and return its handle.
      */
     async createRecordingFile(startAtMs: number, container: ContainerFormat): Promise<FileSystemFileHandle> {
-        const dirHandle = await this.getDirectory()
+        const dirHandle = await navigator.storage.getDirectory()
         const ext = containerExtension(container)
         const fileName = Configuration.filename(startAtMs, ext)
         return dirHandle.getFileHandle(fileName, { create: true })
@@ -25,7 +16,7 @@ export class FileManager {
      * Create an audio separation file in OPFS.
      */
     async createAudioFile(fileName: string): Promise<FileSystemFileHandle> {
-        const dirHandle = await this.getDirectory()
+        const dirHandle = await navigator.storage.getDirectory()
         return dirHandle.getFileHandle(fileName, { create: true })
     }
 }
